6b5463228af3db13829cbd4576716dc746fd6564
fanpero87 <FAb1hn0!>
2022-03-13 11:26:15 -0400

    updating the layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
 	darkMode: "class",
 	content: ["*.html", "./components/**/*.{html,js}", "./src/**/*.{html,js}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"dark-color": "#d0d066",
+				"dark-bg": "#333",
+				"light-color": "#FF4500",
+			},
+		},
 	},
 	plugins: [],
 };
